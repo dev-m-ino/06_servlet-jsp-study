213ef47827d51c2d8517d4115be4f9930d97ce7a
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-04-28 17:22:58 +0900

    0428 수업

---
+++ seoyeon-servlet-jsp-workspace/01_servlet/src/main/java/com/podoseee/servlet/section01/lifecycle/LifeCycleServlet.java
@@ -0,0 +1,58 @@
+package com.podoseee.servlet.section01.lifecycle;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/*
+    * 해당 Servlet의 url mapping
+    - /lifecycle 요청시 실행되도록
+    - http://localhost:8080/servlet/lifecycle    url로 요청해야 작동됨
+ */
+
+public class LifeCycleServlet extends HttpServlet {
+
+    public LifeCycleServlet(){
+        System.out.println("LifeCycleServlet - 생성자 실행");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("LifeCycleServlet - init() 메소드 실행");
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("LifeCycleServlet - doGet() 메소드 실행");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("LifeCycleServlet - doPost() 메소드 실행");
+    }
+     
+/*
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("LifeCycleServlet - service() 메소드 실행");
+
+        switch(req.getMethod().toUpperCase()){
+            case "GET":
+                System.out.println("=> GET 방식으로 요청됨");
+                break;
+            case "POST":
+                System.out.println("=> POST 방식으로 요청됨");
+                break;
+        }
+    }
+*/
+    @Override
+    public void destroy() {
+        System.out.println("LifeCycleServlet - destroy() 메소드 실행");
+    }
+}
---
+++ seoyeon-servlet-jsp-workspace/01_servlet/src/main/java/com/podoseee/servlet/section04/forward/MemberRegistFormServlet.java
@@ -0,0 +1,24 @@
+package com.podoseee.servlet.section04.forward;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/registForm.me")
+public class MemberRegistFormServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 단순페이지(/member/memberRegist.html) 이동
+        req.getRequestDispatcher("/member/memberRegist.html").forward(req, resp);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    
+}
